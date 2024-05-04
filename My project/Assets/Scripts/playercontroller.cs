aa721dc3e9f24e26ebcba823e4780fab1073919b
mekayekim <mertkaganyaman2020@gmail.com>
2024-05-04 16:00:32 +0300

    a

---
+++ My project/Assets/Scripts/playercontroller.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class mainplayercontroler : MonoBehaviour
+{
+    public  Animator animator; // Karakterin Animator bileşeni
+    Rigidbody2D rb; // Karakterin Rigidbody2D bileşeni
+    Vector3 velocity;
+    float speedAmount = 5f;
+        
+
+    void Start()
+    {
+        // Rigidbody2D bileşenini alıyoruz
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0f);
+        transform.position += velocity*speedAmount*Time.deltaTime;
+        animator.SetFloat("speed",Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.rotation = Quaternion.Euler(0f,180f,0f);
+        }
+        else if(Input.GetAxisRaw("Horizontal") == 1)
+        {
+           transform.rotation = Quaternion.Euler(0f,0f,0f); 
+        }
+    }
+}
