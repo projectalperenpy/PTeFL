4624fcf27915cdda35ac78ae9ee322433e63c25f
mekayekim <mertkaganyaman2020@gmail.com>
2024-05-05 02:12:28 +0300

    a

--- My project/Assets/Scripts/playercontroller.cs
+++ My project/Assets/Scripts/playercontroller.cs
@@ -1,26 +1,52 @@
+﻿using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
     private Rigidbody2D body;
+    private Animator anim;
+    private BoxCollider2D boxCollider;
+    private float horizontalInput;
 
-    private void Awake()
+     private void Awake()
     {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
-
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        horizontalInput = Input.GetAxis("Horizontal");
 
+        //Flip player when moving left-right
         if (horizontalInput > 0.01f)
             transform.localScale = Vector3.one;
-        else if (horizontalInput < 0.01f)
+        else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        //Set animator parameters
+        anim.SetBool("run", horizontalInput != 0);
+
+        private void Jump()
+       {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        
+
+            if (horizontalInput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+
+        }
+        private bool canAttack()
+       {
+            return horizontalInput == 0 && isGrounded() && !onWall();
+       }
     }
-}
