3739f428bf426635eb7e4a673d27a87c97146a61
mekayekim <mertkaganyaman2020@gmail.com>
2024-05-05 08:55:59 +0300

    a

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float speed;
+
     private Rigidbody2D body;
     private Animator anim;
+    private bool grounded;
+
 
     private void Awake ()
     {
@@ -18,16 +21,33 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
         if (horizontalInput > 0.01f)
             transform.localScale = new Vector3(4, 4, 4);
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-4, 4, 4);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
 
         anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+
+
     }
+
+    private void Jump()
+    { 
+        body.velocity = new Vector2 (body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
 }
