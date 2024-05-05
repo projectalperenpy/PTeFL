afdd68d574510d97aee2986003aa53f5a08fa8c0
Alperen <alperenittf@gmail.com>
2024-05-05 14:33:15 +0300

    Commit

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -3,42 +3,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     [SerializeField] private float speed;
-
     private Rigidbody2D body;
     private Animator anim;
+    private bool grounded;
 
-
-
-    private void Awake ()
+    private void Awake()
     {
-        body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
     }
 
-    
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
         if (horizontalInput > 0.01f)
-            transform.localScale = new Vector3(4, 4, 4);
+        {
+            transform.localScale = new Vector3((float)3.853515, (float)3.588508, (float)3.117731);
+        }
         else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-4, 4, 4);
+        {
+            transform.localScale = new Vector3(-(float)3.853515, (float)3.588508, (float)3.117731);
+        }
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed); ;
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
 
         anim.SetBool("run", horizontalInput != 0);
-
-
+        anim.SetBool("grounded", grounded);
 
     }
 
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
 
-
-
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
 }
