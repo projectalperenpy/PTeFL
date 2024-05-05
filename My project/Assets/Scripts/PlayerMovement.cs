b338366b09dfed09642a9e0398cf2c3e4f3f7ea0
mekayekim <mertkaganyaman2020@gmail.com>
2024-05-05 03:34:14 +0300

    a

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    private void Awake ()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
-        
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
